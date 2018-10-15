b1bc0a1581d54f5f9cb8aeaac1a6814270d348c2
Mareco Nabors <naborsmarico99@gmail.com>
2018-10-15 11:13:38 -0500

    test_cases

--- pages/tests.py
+++ pages/tests.py
@@ -29,3 +29,28 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertNotContains(
             response, 'Hi there! I should not be on the page.')
+
+# TEST IN THE ABOUT PAGE
+
+class AboutTestPage(SimpleTestCase):
+
+    def testing_about_page_code(self):
+        response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
+
+    def testing_url_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEquals(response.status_code, 200)
+
+    def testing_for_correct_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+    def testing_about_page_correct_html(self):
+        response=self.client.get('/about/')
+        self.assertContains(response, '<h1>About page</h1>')
+
+    def testing_about_page_if_no_contain(self):
+        response = self.client.get('')
+        self.assertNotContains(response, 'If you reading this then you are not on the page.')
